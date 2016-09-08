c3297516a0b2c0457d1589ba36496d82a5f983df
gmaor88 <gmaor88@gmail.com>
2016-09-08 19:13:44 +0300

    Show move list - fxml and controller.

---
+++ src/GUI/ShowMoveListController.java
@@ -0,0 +1,14 @@
+package GUI;
+
+import javafx.scene.control.ListView;
+
+/**
+ * Created by Maor Gershkovitch on 9/8/2016.
+ */
+public class ShowMoveListController {
+    private ListView MoveListListView;
+
+    public ListView getMoveListListView() {
+        return MoveListListView;
+    }
+}
